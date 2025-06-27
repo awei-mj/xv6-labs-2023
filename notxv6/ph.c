806c70ca3e85b616ed7b95b4a0cae903535e94a2
awei-mj <2510558223@qq.com>
2025-06-27 23:46:17 +0800

    lab_thread parallel hash table

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -14,6 +14,7 @@ struct entry {
   struct entry *next;
 };
 struct entry *table[NBUCKET];
+pthread_mutex_t lock[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
@@ -49,10 +50,14 @@ void put(int key, int value)
   }
   if(e){
     // update the existing key.
+    pthread_mutex_lock(&lock[i]);
     e->value = value;
+    pthread_mutex_unlock(&lock[i]);
   } else {
     // the new is new.
+    pthread_mutex_lock(&lock[i]);
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&lock[i]);
   }
 
 }
@@ -116,6 +121,11 @@ main(int argc, char *argv[])
   assert(NKEYS % nthread == 0);
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
+  }
+
+  for (int i = 0; i < NBUCKET; i++) {
+    table[i] = NULL;
+    assert(pthread_mutex_init(&lock[i], NULL) == 0);
   }
 
   //
